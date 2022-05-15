5da5a7616b006b5ba4330144908b38b1d0ccbcd5
Adji Arioputro <adji.arioputro@invensity.com>
2022-05-15 17:09:38 +0200

    FEAT: Add cleanup track duplication function

--- compose.py
+++ compose.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
+from email import message
 from pathlib import Path
 import shutil
 from typing import Generator, List, Optional, Union
 
 from mido import MidiFile, MetaMessage
 
-def get_all_midis(optional_path: Optional[Union[str, Path]]= None) -> List[Path]:
+def get_all_midis(optional_path: Optional[Union[str, Path]]= None) -> Generator[Path, None, None]:
     if optional_path:
         midi_files = Path(optional_path).rglob("*.mid")
     else:
@@ -27,6 +28,21 @@ def clean_broken_midi(midi_files_gen: Generator[Path, None, None], optional_arch
                 broken_archive_folder.mkdir(parents=True, exist_ok=True)
             shutil.move(midi_path, broken_archive_folder / midi_path.name)
     return cleaned_midi_paths
+
+def clean_duplicate_tracks(midi_file: MidiFile, optional_archive_path: Optional[Union[str, Path]]= None) -> List[Path]:
+    message_numbers = []
+    duplicates = []
+    
+    for track in midi_file.tracks:
+        if len(track) in message_numbers:
+            duplicates.append(track)
+        else:
+            message_numbers.append(len(track))
+    
+    for track in duplicates:
+        midi_file.tracks.remove(track)
+    
+    midi_file.save("")    
 
 if __name__ == "__main__":
     cleaned_midi_files = clean_broken_midi(get_all_midis())
