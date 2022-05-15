965a9e9770132d1f99644cfdbb1ec9b3eff80142
Adji Arioputro <adji.arioputro@invensity.com>
2022-05-15 17:36:06 +0200

    BLD: Create placeholder setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,107 @@
+<<<<<<< HEAD
+=======
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from sphinx.setup_command import BuildDoc
+import pathlib
+
+# Get the long description from the README file
+here = pathlib.Path(__file__).parent.resolve()
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
+cmdclass = {"build_sphinx": BuildDoc}
+
+name = "inv_ai_music_composer", # Required
+version = "0.0.1", # Required
+
+setup(
+    name=name,
+    version=version,
+    description="Passion Project, AI Music Composer", # Optional
+    long_description=long_description, # Optional
+    long_description_content_type="text/markdown",  # Optional
+    url="",  # Optional
+    author="Adji Arioputro",  # Optional
+    author_email="adji.arioputro@invensity.com",  # Optional
+    classifiers=[  # Optional
+        "Development Status :: 2 - PreAlpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    keywords="sample, magenta, development, tensorflow",  # Optional
+    package_dir={"": "src"},  # Optional
+    packages=find_packages(where="src"),  # Required
+    python_requires=">=3.7, <3.9",
+    # This field lists other packages that your project depends on to run.
+    # Any package you put here will be installed by pip when your project is
+    # installed, so they must be valid existing projects.
+    #
+    # For an analysis of "install_requires" vs pip's requirements files see:
+    # https://packaging.python.org/discussions/install-requires-vs-requirements/
+    install_requires=[
+        "mido",
+        "magenta",
+        "tensorflow"
+        ],  # Optional
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). Users will be able to install these using the "extras"
+    # syntax, for example:
+    #
+    #   $ pip install sampleproject[dev]
+    #
+    # Similar to `install_requires` above, these must be valid existing
+    # projects.
+    extras_require={  # Optional
+        "test": [
+            "pytest",
+            ],
+    },
+    # If there are data files included in your packages that need to be
+    # installed, specify them here.
+    package_data={  # Optional
+        "sample": ["package_data.dat"],
+    },
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/distutils/setupscript.html#installing-additional-files
+    #
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[("my_data", ["data/data_file"])],  # Optional
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # `pip` to create the appropriate form of executable for the target
+    # platform.
+    #
+    # For example, the following would provide a command called `sample` which
+    # executes the function `main` from this package when invoked:
+    entry_points={  # Optional
+        "console_scripts": [
+            "sample=sample:main",
+        ],
+    },
+    # List additional URLs that are relevant to your project as a dict.
+    #
+    # This field corresponds to the "Project-URL" metadata fields:
+    # https://packaging.python.org/specifications/core-metadata/#project-url-multiple-use
+    #
+    # Examples listed include a pattern for specifying where the package tracks
+    # issues, where the source is hosted, where to say thanks to the package
+    # maintainers, and where to support the project financially. The key is
+    # what's used to render the link text on PyPI.
+    project_urls={  # Optional
+        "Bug Reports": "https://github.com/pypa/sampleproject/issues",
+        "Funding": "https://donate.pypi.org",
+        "Say Thanks!": "http://saythanks.io/to/example",
+        "Source": "https://github.com/pypa/sampleproject/",
+    },
+    command_options={
+        "build_sphinx": {
+            "project": ("setup.py", name),
+            "version": ("setup.py", version),
+        }
+    },
+)
+>>>>>>> b6514ee (BLD: Add Sphinx for build docs in setup.py)
