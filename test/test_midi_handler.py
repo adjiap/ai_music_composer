7c003a48140a182a716bad9b098c54b9cedb14bd
adjiap <20769489+adjiap@users.noreply.github.com>
2022-05-29 14:25:35 +0200

    TST: Add placeholder tests to MidiHandler class

--- test/test_midi_handler.py
+++ test/test_midi_handler.py
@@ -1,1 +1,21 @@
 # -*- coding: utf-8 -*-
+
+import pytest
+from midi_handler import get_all_midis
+
+class TestMidiHandler():
+    
+    @pytest.mark.skip(reason="Not yet implemented")
+    def test_get_all_midis(self,):
+        raise NotImplementedError
+
+    @pytest.mark.skip(reason="Not yet implemented")
+    def test_clean_duplicate_tracks(self,):
+        raise NotImplementedError
+    
+    @pytest.mark.skip(reason="Not yet implemented")
+    def test_clean_broken_midi(self,):
+        raise NotImplementedError
+    
+if __name__ == '__main__':
+    pytest.main()
