8fc6808f57b0dee123946926dc40a76aaa8722ce
adjiap <20769489+adjiap@users.noreply.github.com>
2022-05-29 14:52:08 +0200

    MAINT: Move midi functions to midi_handler

--- compose.py
+++ compose.py
@@ -1,51 +1,10 @@
 # -*- coding: utf-8 -*-
-from email import message
-from pathlib import Path
-import shutil
-from typing import Generator, List, Optional, Union
+from midi_handler import MidiHandler
 
 from mido import MidiFile, MetaMessage
 
-def get_all_midis(optional_path: Optional[Union[str, Path]]= None) -> Generator[Path, None, None]:
-    if optional_path:
-        midi_files = Path(optional_path).rglob("*.mid")
-    else:
-        midi_files = Path.cwd().rglob("*.mid")
-    return midi_files
-
-def clean_broken_midi(midi_files_gen: Generator[Path, None, None], optional_archive_path: Optional[Union[str, Path]]= None) -> List[Path]:
-    cleaned_midi_paths = [] # type: List[Path]
-    while True:
-        midi_path = next(midi_files_gen, "end")
-        if midi_path == "end":
-            break
-        try:
-            MidiFile(midi_path, clip=True)
-            cleaned_midi_paths.append(midi_path)
-        except EOFError:
-            broken_archive_folder = optional_archive_path if optional_archive_path else Path(__file__).parent / "midi_src" / "broken_midi"
-            if not broken_archive_folder.exists():
-                broken_archive_folder.mkdir(parents=True, exist_ok=True)
-            shutil.move(midi_path, broken_archive_folder / midi_path.name)
-    return cleaned_midi_paths
-
-def clean_duplicate_tracks(midi_file: MidiFile, optional_archive_path: Optional[Union[str, Path]]= None) -> List[Path]:
-    message_numbers = []
-    duplicates = []
-    
-    for track in midi_file.tracks:
-        if len(track) in message_numbers:
-            duplicates.append(track)
-        else:
-            message_numbers.append(len(track))
-    
-    for track in duplicates:
-        midi_file.tracks.remove(track)
-    
-    midi_file.save("")    
-
 if __name__ == "__main__":
-    cleaned_midi_files = clean_broken_midi(get_all_midis())
+    cleaned_midi_files = MidiHandler.clean_broken_midi(MidiHandler.get_all_midis())
     for midi_path in cleaned_midi_files:
         midi_file = MidiFile(midi_path, clip=True)
         for msg in midi_file.tracks[0]:
